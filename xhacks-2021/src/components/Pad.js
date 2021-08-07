9ff309477e2fb635ba3213405b79e23fb5e72a3f
Endreas Yohannes <endreas@hotmail.ca>
2021-08-06 23:09:43 -0400

    temp commit

---
+++ xhacks-2021/src/components/Pad.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Pad extends Component {
+    state = {
+        mode = 'default',
+
+    }
+
+    changeMode(button) {
+        switch(button){
+            
+        }
+    }
+
+    render() { 
+        return (
+        <div>
+            <button>1</button>
+        </div>);
+    }
+}
